9327db2b776a08b7f786332375903154d152a5f5
johananSantillan <johanan-santi10@hotmail.com>
2023-09-25 13:09:16 -0500

    Update Java (1.8 to 17) and Spring boot (2.4.5 to 3.0.0)

--- sevuelo-backend/src/main/java/ec/sevolutivo/sevuelos/sevuelos/domain/Request.java
+++ sevuelo-backend/src/main/java/ec/sevolutivo/sevuelos/sevuelos/domain/Request.java
@@ -2,9 +2,9 @@ package ec.sevolutivo.sevuelos.sevuelos.domain;
 
 import ec.sevolutivo.sevuelos.sevuelos.domain.enumeration.RequestStatus;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.io.Serializable;
 
 /**
